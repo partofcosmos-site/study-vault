7f39e4495af6997453d7de7707f0da40e5cbc4be
Partofcosmos1-re <dip868610@gmail.com>
2025-12-30 02:23:24 +0530

    Move problem database to client/src to fix Vercel build

--- client/src/app/api/problems/[id]/route.ts
+++ client/src/app/api/problems/[id]/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import problems from '../../../../../../server/data/problems.json';
+import problems from '../../../../data/problems.json';
 
 export async function GET(
   request: Request,
